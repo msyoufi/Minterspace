82a936a30af2545efe2d2e1889f8aee83738e98f
msyoufi <msyoufi.dev@gmail.com>
2025-03-29 17:53:18 +0100

    feat: add app theme toggle.

--- frontend/src/components/header/theme-button.tsx
+++ frontend/src/components/header/theme-button.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function ThemeButton() {
+  const [isDarkTheme, setIsDarkTheme] = useState(getIsDarkTheme);
+
+  useEffect(() => toggleAppTheme(isDarkTheme), [isDarkTheme]);
+
+  function getIsDarkTheme(): boolean {
+    return JSON.parse(localStorage.getItem('isDarkTheme') || 'true');
+  }
+
+  function handleClick(): void {
+    setIsDarkTheme(prev => !prev);
+  }
+
   return (
-    <button className=''>
-      <i className="bi bi-sun-fill"></i>
-      <i className="bi bi-moon-fill"></i>
+    <button className='icon-button theme-button' onClick={handleClick}>
+      {isDarkTheme
+        ? <i className="bi bi-sun-fill"></i>
+        : <i className="bi bi-moon-fill"></i>
+      }
     </button>
   );
+}
+
+function toggleAppTheme(isDarkTheme: boolean): void {
+  if (isDarkTheme)
+    document.body.classList.remove('light-theme');
+  else
+    document.body.classList.add('light-theme');
+
+  localStorage.setItem('isDarkTheme', JSON.stringify(isDarkTheme));
 }
