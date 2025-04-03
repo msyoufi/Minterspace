d1377234402ad25b410c06ab118341302eb5b4db
msyoufi <msyoufi.dev@gmail.com>
2025-04-03 22:50:57 +0200

    feat: add app navigation in the header.

--- frontend/src/app/components/header/navigation/navigation.component.ts
+++ frontend/src/app/components/header/navigation/navigation.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ms-navigation',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  router = inject(Router);
 
+  isNavMenuOpen = signal(false);
+
+  toggleNavMenu(): void {
+    this.isNavMenuOpen.set(!this.isNavMenuOpen());
+  }
 }
