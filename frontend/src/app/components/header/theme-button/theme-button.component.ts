4f23f18ba5e0c49c0cee9d3798e251bc592e82c0
msyoufi <msyoufi.dev@gmail.com>
2025-04-03 22:10:15 +0200

    feat: add color theme toggle button.

--- frontend/src/app/components/header/theme-button/theme-button.component.ts
+++ frontend/src/app/components/header/theme-button/theme-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'ms-theme-button',
@@ -7,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrl: './theme-button.component.scss'
 })
 export class ThemeButtonComponent {
+  isDarkTheme = signal(true);
 
+  constructor() {
+    this.setInitialTheme();
+    effect(() => this.toggleAppTheme());
+  }
+
+  setInitialTheme(): void {
+    const isDarkTheme = JSON.parse(localStorage.getItem('isDarkTheme') || 'true');
+    this.isDarkTheme.set(isDarkTheme);
+  }
+
+  toggleColorTheme(): void {
+    this.isDarkTheme.set(!this.isDarkTheme());
+  }
+
+  toggleAppTheme(): void {
+    if (this.isDarkTheme())
+      document.body.classList.remove('light-theme');
+    else
+      document.body.classList.add('light-theme');
+
+    localStorage.setItem('isDarkTheme', JSON.stringify(this.isDarkTheme()));
+  }
 }
