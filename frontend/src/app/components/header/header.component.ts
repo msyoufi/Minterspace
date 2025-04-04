cad477a2bbf3aadf8c2d4f82bc8ece9d03804c81
msyoufi <msyoufi.dev@gmail.com>
2025-04-04 20:49:36 +0200

    feat: make header response to screen resize.

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { GlobalMarketBarComponent } from "./global-market-bar/global-market-bar.component";
 import { AccountButtonComponent } from "./account-button/account-button.component";
 import { NavigationComponent } from "./navigation/navigation.component";
 import { HeaderSearchBarComponent } from "./header-search-bar/header-search-bar.component";
 import { ThemeButtonComponent } from "./theme-button/theme-button.component";
 import { AppLogoComponent } from "../../shared/components/app-logo.component";
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'ms-header',
@@ -13,5 +15,18 @@ import { AppLogoComponent } from "../../shared/components/app-logo.component";
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  breakpointObserver = inject(BreakpointObserver);
+  destroyRef = inject(DestroyRef);
 
+  isSmallScreen = signal(false);
+
+  constructor() {
+    this.subscribeToScreenResize();
+  }
+
+  subscribeToScreenResize(): void {
+    this.breakpointObserver.observe(['(max-width: 850px)'])
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(result => this.isSmallScreen.set(result.matches));
+  }
 }
