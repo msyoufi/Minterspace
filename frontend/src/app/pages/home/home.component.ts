0ab24df80af585fb412fd604b8047ff995f665ea
msyoufi <msyoufi.dev@gmail.com>
2025-04-05 19:00:45 +0200

    WIP: add the coinCategoryPanels component to the home page.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CoinsListComponent } from "../../shared/components/coins-list/coins-list.component";
 import { CoingeckoService } from '../../shared/services/coingecko.service';
+import { CategoryPanelsComponent } from "./category-panels/category-panels.component";
 
 @Component({
   selector: 'ms-home',
-  imports: [CoinsListComponent],
+  imports: [CoinsListComponent, CategoryPanelsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   coinService = inject(CoingeckoService);
+  currentCategory = signal<CoinCategory | GlobalMarket | null>(this.coinService.coinCategories()[0]);
 
   constructor() {
     this.coinService.getCoins();
