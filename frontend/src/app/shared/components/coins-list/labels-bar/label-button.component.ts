d312e39bb1b80999fe496df464f977ade34f455e
msyoufi <msyoufi.dev@gmail.com>
2025-04-05 09:07:38 +0200

    WIP: create a label button component.

---
+++ frontend/src/app/shared/components/coins-list/labels-bar/label-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, input, model, output } from '@angular/core';
+
+@Component({
+  selector: 'ms-label-button',
+  imports: [],
+  template: `
+    <button (click)="changeSortKey.emit()" [class]="className()">
+      {{content()}}
+    </button>
+  `,
+  styleUrls: [
+    './labels-bar.component.scss',
+    '../coins-list.component.scss'
+  ]
+
+})
+export class LabelButtonComponent {
+  content = input.required<string>();
+  className = input<string>('');
+  changeSortKey = output<void>();
+}
