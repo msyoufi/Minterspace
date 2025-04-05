69c21fc9f129c9685757d2fd726b3f8674a84880
msyoufi <msyoufi.dev@gmail.com>
2025-04-05 23:21:46 +0200

    WIP: add the sparkline to the coinbar

--- frontend/src/app/shared/components/coins-list/coin-bar/coin-bar.component.ts
+++ frontend/src/app/shared/components/coins-list/coin-bar/coin-bar.component.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import { WatchlistStarComponent } from "../../watchlist-star.component";
 import { CommonModule } from '@angular/common';
 import { AbsolutPipe } from '../../../pipes/absolut.pipe';
+import { SparklineComponent } from "../../sparkline.component";
 
 @Component({
   selector: 'ms-coin-bar',
-  imports: [RouterLink, CommonModule, AbsolutPipe, WatchlistStarComponent],
+  imports: [RouterLink, CommonModule, AbsolutPipe, WatchlistStarComponent, SparklineComponent],
   templateUrl: './coin-bar.component.html',
   styleUrls: [
     './coin-bar.component.scss',
