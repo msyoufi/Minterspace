f91f751842d1e11710752aa8cf671b3b44ed9493
msyoufi <msyoufi.dev@gmail.com>
2025-04-05 21:48:24 +0200

    feat: create the app footer.

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppLogoComponent } from "../../shared/components/app-logo.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ms-footer',
-  imports: [],
+  imports: [AppLogoComponent, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
