55f284ff4a5d4070ed6abd5941cec02d31036d3f
msyoufi <msyoufi.dev@gmail.com>
2025-04-07 00:03:57 +0200

    WIP: add the coin details header

--- frontend/src/app/pages/coin-details/coin-details-header/coin-details-header.component.ts
+++ frontend/src/app/pages/coin-details/coin-details-header/coin-details-header.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { AbsolutPipe } from '../../../shared/pipes/absolut.pipe';
 
 @Component({
   selector: 'ms-coin-details-header',
-  imports: [],
+  imports: [CommonModule, AbsolutPipe],
   templateUrl: './coin-details-header.component.html',
   styleUrl: './coin-details-header.component.scss'
 })
