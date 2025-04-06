dc379ea970bd748a711d4231bc85ce492c660bc4
msyoufi <msyoufi.dev@gmail.com>
2025-04-06 19:08:38 +0200

    WIP: request the coin data on navigation to coin Deatils.

--- frontend/src/app/pages/coin-details/coin-details.component.ts
+++ frontend/src/app/pages/coin-details/coin-details.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal, DestroyRef } from '@angular/core';
+import { Component, inject, signal, DestroyRef, Input } from '@angular/core';
 import { CoingeckoService } from '../../shared/services/coingecko.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import coinDetailsData from '../../shared/mock/coin-details.json';
+
 @Component({
   selector: 'ms-coin-details',
   imports: [],
@@ -14,21 +16,22 @@ export class CoinDetailsComponent {
   isLoading = signal<boolean>(true);
   coin = signal<CoinDetails | null>(null);
 
-
-  constructor() {
-    this.getCoinData();
+  @Input() set id(coinId: string) {
+    console.log(coinId)
+    this.getCoinData(coinId);
   }
 
-  getCoinData(): void {
+  getCoinData(coinId: string): void {
     this.isLoading.set(true);
 
-    this.coinService.getCoinDetails('bitcoin')
+    this.coinService.getCoinDetails(coinId)
       .pipe(takeUntilDestroyed(this.destroyRef))
       .subscribe(coin => {
-        console.log(coin)
+        this.coin.set(coin);
+        this.isLoading.set(false);
       });
 
-    // this.coin.set(null);
+    // this.coin.set(coinDetailsData);
     // this.isLoading.set(false);
   }
 }
