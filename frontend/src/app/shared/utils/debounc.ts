60fd6cdd89d2211727baff6aa470de4464024fd5
msyoufi <msyoufi.dev@gmail.com>
2025-04-08 23:13:16 +0200

    refactor: delete unneccessary debounce function.

--- frontend/src/app/shared/utils/debounc.ts
+++
@@ -1,8 +0,0 @@
-export default function debounce<T extends (...args: any[]) => R, R>(fn: T, delay: number = 400) {
-  let timerId: ReturnType<typeof setTimeout>;
-
-  return (...args: Parameters<T>) => {
-    clearTimeout(timerId);
-    timerId = setTimeout(() => fn(...args), delay);
-  }
-}
