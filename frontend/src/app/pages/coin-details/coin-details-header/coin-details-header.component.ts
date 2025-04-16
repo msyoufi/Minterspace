09f6b91d926a56d64e324e0992acc443e4d14887
msyoufi <msyoufi.dev@gmail.com>
2025-04-16 14:08:59 +0200

    WIP: implement the watchlist star component functionality.

--- frontend/src/app/pages/coin-details/coin-details-header/coin-details-header.component.ts
+++ frontend/src/app/pages/coin-details/coin-details-header/coin-details-header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { AbsolutPipe } from '../../../shared/pipes/absolut.pipe';
+import { WatchlistStarComponent } from '../../../shared/components/watchlist-star.component';
 
 @Component({
   selector: 'ms-coin-details-header',
-  imports: [CommonModule, AbsolutPipe],
+  imports: [CommonModule, AbsolutPipe, WatchlistStarComponent],
   templateUrl: './coin-details-header.component.html',
   styleUrl: './coin-details-header.component.scss'
 })
--- frontend/src/app/shared/components/watchlist-star.component.ts
+++ frontend/src/app/shared/components/watchlist-star.component.ts
@@ -1,23 +1,63 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
+import { WatchlistService } from '../services/watchlist.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MatTooltip } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ms-watchlist-star',
-  imports: [],
+  imports: [MatTooltip, MatProgressSpinner],
   template: `
-    <button class="icon-button" (click)="onStarClick($event)">
-      <i class='bi bi-star'></i>
-    </button>
+    <div>
+      @if(isLoading()) {
+      <mat-spinner [diameter]="20" [strokeWidth]="2" />
+      } @else {
+      <button class="icon-button" (click)="onStarClick($event)"
+        [matTooltip]="(isInWatchlist() ? 'Remove From ' : 'Add To ') + relevantWatchlist()?.name + ' Watchlist'">
+        <i [class]="'bi bi-star' + (isInWatchlist() ? '-fill' : '')"></i>
+      </button>
+      }
+    </div>
   `,
-  styles: ''
+  styles: ` 
+    div {
+      text-align: center;
+    }
+  `
 })
 export class WatchlistStarComponent {
+  watchlistService = inject(WatchlistService);
+  router = inject(Router);
+
+  relevantWatchlist = this.router.url.includes('watchlist')
+    ? this.watchlistService.currentWatchlist$
+    : this.watchlistService.mainWatchlist;
+
   coinId = input.required<string>();
+  isInWatchlist = computed(() => this.relevantWatchlist()?.coins.includes(this.coinId()));
+
+  isLoading = signal(false);
 
   onStarClick(e: MouseEvent): void {
     e.stopPropagation();
     e.preventDefault();
-    console.log(this.coinId());
-    // TODO
+
+    const watchlist = this.relevantWatchlist();
+
+    if (!watchlist) return;
+
+    this.updateWatchlistCoins(watchlist);
+  }
+
+  async updateWatchlistCoins(watchlist: Watchlist): Promise<void> {
+    this.isLoading.set(true);
+
+    const coins = this.isInWatchlist()
+      ? watchlist.coins.filter(id => id !== this.coinId())
+      : [...watchlist.coins, this.coinId()];
+
+    await this.watchlistService.updateWatchlist(this.relevantWatchlist()!.id, { coins });
+    
+    this.isLoading.set(false);
   }
 }
