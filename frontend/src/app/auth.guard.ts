cf4c86e4a00a196e36aed92a9dbf7b0ab94c3875
msyoufi <msyoufi.dev@gmail.com>
2025-04-17 22:09:51 +0200

    WIP: navigate to home on cannot activate route.

--- frontend/src/app/auth.guard.ts
+++ frontend/src/app/auth.guard.ts
@@ -1,13 +1,17 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
 
   if (!authService.user$()) {
     const url = state.url.includes('account') ? '/' : state.url;
+
     authService.openAuthModal(url);
+    router.navigateByUrl('/');
+
     return false;
   }
 
