062cfcaaef5e175c170ee0d9bc3fc31cf0cd1ad0
msyoufi <msyoufi.dev@gmail.com>
2025-04-23 20:01:10 +0200

    WIP: add a snackbar component/service.

---
+++ frontend/src/app/shared/services/snack-bar.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackBarService {
+  private snackBar = inject(MatSnackBar);
+
+  public showMessage(
+    message: string,
+    color: 'primary' | 'red' | 'green' = 'primary',
+    duration = 3000
+  ): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, undefined, {
+      duration,
+      panelClass: ['snack-bar', color]
+    });
+  }
+}
