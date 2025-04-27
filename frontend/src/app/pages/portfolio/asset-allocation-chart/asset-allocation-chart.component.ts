db73114510a90fe847094224f0520cabc3ed38ae
msyoufi <msyoufi.dev@gmail.com>
2025-04-27 21:09:11 +0200

    WIP: implement the asset allocation chart using Apache Echarts library.

--- frontend/src/app/pages/portfolio/asset-allocation-chart/asset-allocation-chart.component.ts
+++ frontend/src/app/pages/portfolio/asset-allocation-chart/asset-allocation-chart.component.ts
@@ -1,11 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, effect, ElementRef, inject, OnDestroy, viewChild } from '@angular/core';
+import { ECharts, EChartsOption, init } from 'echarts';
+import { PortfolioService } from '../../../shared/services/portfolio.service';
 
 @Component({
   selector: 'ms-asset-allocation-chart',
   imports: [],
-  templateUrl: './asset-allocation-chart.component.html',
-  styleUrl: './asset-allocation-chart.component.scss'
+  template: '<div aria-label="Doughnut Chart Crypto Asset Allocation" role="img" #chartContainer></div>',
+  styles: `
+  div {
+    width: 100%;
+    height: 270px;
+  }`,
+  host: {
+    '(window:resize)': 'onWindowResize()'
+  }
 })
-export class AssetAllocationChartComponent {
+export class AssetAllocationChartComponent implements OnDestroy {
+  portfolioService = inject(PortfolioService);
 
+  chartContainer = viewChild.required<ElementRef<HTMLDivElement>>('chartContainer');
+  chart: ECharts | undefined;
+
+  constructor() {
+    effect(() => this.onChartDataChange());
+  }
+
+  onChartDataChange(): void {
+    const chartData = this.portfolioService.currentPortfolioData$()?.allocation_chart;
+    if (!chartData) return;
+
+    this.drawChart(chartData);
+  }
+
+  drawChart(chartData: AllocationChartDataPoint[]): void {
+    this.chart?.dispose();
+
+    this.chartOptions.dataset = { source: chartData };
+    this.chart = init(this.chartContainer().nativeElement);
+    this.chart.setOption(this.chartOptions);
+  }
+
+  onWindowResize(): void {
+    this.chart?.resize();
+  }
+
+  ngOnDestroy(): void {
+    this.chart?.dispose();
+  }
+
+  chartOptions: EChartsOption = {
+    tooltip: {
+      trigger: 'item',
+      formatter: function (params) {
+        //@ts-ignore
+        const { marker, name, value, percent } = params;
+        const inCurrency = value.value.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+
+        return `
+          ${marker} <b>${name}</b><br/>
+          <b>${inCurrency}</b> | <b>${Math.round(percent)}%</b>
+        `;
+      }
+    },
+    legend: {
+      type: 'scroll',
+      orient: 'vertical',
+      icon: 'circle',
+      left: 21,
+      top: 60,
+      textStyle: {
+        color: 'var'
+      }
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: ['80%', '60%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderWidth: 0
+        },
+        label: {
+          show: false,
+        }
+      }
+    ]
+  };
 }
