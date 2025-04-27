80879173bc8c15d45972dd869acf3c8d86f34c82
msyoufi <msyoufi.dev@gmail.com>
2025-04-27 15:01:03 +0200

    WIP: implement the portfolio stats component.

--- frontend/src/app/pages/portfolio/portfolio-stats-panes/portfolio-stats-panes.component.ts
+++ frontend/src/app/pages/portfolio/portfolio-stats-panes/portfolio-stats-panes.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { PortfolioService } from '../../../shared/services/portfolio.service';
+import { PerformancePaneComponent } from '../../../shared/components/performance-pane/performance-pane.component';
+import { RouterLink } from '@angular/router';
+import { TotalsPaneComponent } from './totals-pane/totals-pane.component';
 
 @Component({
   selector: 'ms-portfolio-stats-panes',
-  imports: [],
+  imports: [PerformancePaneComponent, RouterLink, TotalsPaneComponent],
   templateUrl: './portfolio-stats-panes.component.html',
   styleUrl: './portfolio-stats-panes.component.scss'
 })
 export class PortfolioStatsPanesComponent {
+  portfolioService = inject(PortfolioService);
 
+  stats = signal<PortfolioStats | null>(null);
+  topGainer = signal<Asset | null>(null);
+  worstGainer = signal<Asset | null>(null);
+
+  constructor() {
+    effect(() => this.getStats());
+  }
+
+  getStats(): void {
+    const portfolioData = this.portfolioService.currentPortfolioData$();
+    if (!portfolioData) return;
+
+    const { assets, stats } = portfolioData;
+
+    const topGainer = assets.find(ast => ast.coin_id === stats.top_gainer_id)!;
+    const worstGainer = assets.find(ast => ast.coin_id === stats.worst_gainer_id)!;
+
+    this.stats.set(stats);
+    this.topGainer.set(topGainer);
+    this.worstGainer.set(worstGainer);
+  }
 }
---
+++ frontend/src/app/pages/portfolio/portfolio-stats-panes/totals-pane/totals-pane.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { AbsolutPipe } from '../../../../shared/pipes/absolut.pipe';
+
+@Component({
+  selector: 'ms-totals-pane',
+  imports: [CommonModule, AbsolutPipe],
+  templateUrl: './totals-pane.component.html',
+  styleUrl: '../portfolio-stats-panes.component.scss'
+})
+export class TotalsPaneComponent {
+  total_balance = input.required<number>();
+  total_profit_loss = input.required<number>();
+  total_profit_loss_percentage = input.required<number>();
+}
