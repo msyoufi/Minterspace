9cab0833b10429bc89aef159c205746cd3b694a3
msyoufi <msyoufi.dev@gmail.com>
2025-04-30 20:05:48 +0200

    WIP: implement the asset panes component.

--- frontend/src/app/pages/portfolio/asset-details/asset-panes/asset-panes.component.ts
+++ frontend/src/app/pages/portfolio/asset-details/asset-panes/asset-panes.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AbsolutPipe } from '../../../../shared/pipes/absolut.pipe';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'ms-asset-panes',
-  imports: [],
+  imports: [RouterLink, CommonModule, AbsolutPipe, MatTooltip],
   templateUrl: './asset-panes.component.html',
   styleUrl: './asset-panes.component.scss'
 })
