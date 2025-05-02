94ac0d998a6c813d6a712b62d18ac3b952bda933
msyoufi <msyoufi.dev@gmail.com>
2025-05-02 23:12:12 +0200

    WIP: set up the main layout for the account component.

--- frontend/src/app/pages/account/account.component.ts
+++ frontend/src/app/pages/account/account.component.ts
@@ -1,16 +1,22 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
+import { SecuritySettingsComponent } from './security-settings/security-settings.component';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+
+type AccountTaps = 'profile' | 'security';
 
 @Component({
   selector: 'ms-account',
-  imports: [],
+  imports: [ProfileSettingsComponent, SecuritySettingsComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
   authService = inject(AuthService);
 
-  onLogoutClick(): void {
-    this.authService.logout();
+  tab = signal<AccountTaps>('profile');
+
+  setTab(tab: AccountTaps): void {
+    this.tab.set(tab);
   }
 }
