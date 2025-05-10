3ef1975f08cb57fce0eec7a8582964ca0ebf8f63
msyoufi <msyoufi.dev@gmail.com>
2025-05-10 18:24:16 +0200

    refactor: use a reactive form instead of a template form for consistent Password validation.
    also add the input toggle eye.

--- frontend/src/app/pages/account/security-settings/update-password-form/update-password-form.component.ts
+++ frontend/src/app/pages/account/security-settings/update-password-form/update-password-form.component.ts
@@ -1,13 +1,15 @@
-import { Component, inject, input, signal, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component, inject, signal, viewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatExpansionModule, MatExpansionPanel } from '@angular/material/expansion';
 import { AccountService } from '../../../../shared/services/account.service';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { SnackBarService } from '../../../../shared/services/snack-bar.service';
+import MsValidators from '../../../../shared/utils/ms.validators';
+import { PasswordEyeComponent } from '../../../../shared/components/password-eye.component';
 
 @Component({
   selector: 'ms-update-password-form',
-  imports: [MatExpansionModule, FormsModule, MatProgressSpinner],
+  imports: [MatExpansionModule, ReactiveFormsModule, MatProgressSpinner, PasswordEyeComponent],
   templateUrl: './update-password-form.component.html',
   styleUrl: '../../account.component.scss'
 })
@@ -15,26 +17,31 @@ export class UpdatePasswordFormComponent {
   accountService = inject(AccountService);
   snackbar = inject(SnackBarService);
 
+  form = new FormGroup({
+    newPassword: new FormControl('', [Validators.required, MsValidators.password, Validators.minLength(8)]),
+    currentPassword: new FormControl('', Validators.required)
+  });
+
   panel = viewChild<MatExpansionPanel>('panel');
-  form = viewChild.required<NgForm>('form');
   isLoading = signal(false);
 
   async onSubmit(): Promise<void> {
-    if (this.form().invalid) return;
+    if (this.form.invalid) return;
 
     this.isLoading.set(true);
 
-    const { newPassword, currentPassword } = this.form().value;
-    const result = await this.accountService.updatePassword(newPassword, currentPassword);
+    const { newPassword, currentPassword } = this.form.value;
+    const result = await this.accountService.updatePassword(newPassword!, currentPassword!);
 
     this.isLoading.set(false);
-    console.log(result);
 
-    if (result)
-      this.snackbar.show('Password Changed', 'green');
+    if (!result) return;
+
+    this.snackbar.show('Username Changed', 'green');
+    this.closePanel();
   }
 
-  onCancelClick(): void {
+  closePanel(): void {
     this.panel()?.close()
   }
 }
